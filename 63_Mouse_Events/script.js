9a413162058917b432d350e828090172cb91d5dd
mascinn <makhasinmuhammad03@gmail.com>
2026-07-07 09:34:46 +0700

    learn about mouse events

---
+++ 63_Mouse_Events/script.js
@@ -0,0 +1,21 @@
+// eventlistener = Listen for specific events to create interactive web pages
+//                 events: click, mouseover, mouseout
+//                 .addEventListener(event, callback);
+
+const myBox = document.getElementById("myBox");
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", event => {
+    myBox.style.backgroundColor = "tomato";
+    myBox.textContent = "OUCHH 🤕"
+});
+
+myButton.addEventListener("mouseover", event => {
+    myBox.style.backgroundColor = "yellow";
+    myBox.textContent = "Don't do it 😲";
+});
+
+myButton.addEventListener("mouseout", event => {
+    myBox.style.backgroundColor = "lightgreen";
+    myBox.textContent = "Click Me 😃";
+});
